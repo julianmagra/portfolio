e83475136b1dec046be3c4b84629cbeedc56a173
julianmagra <julianmagraestudio@gmail.com>
2024-05-20 21:46:12 -0300

    TimeLine and flex

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,101 +1,113 @@
 import { FC, ReactElement } from "react";
 import { Link } from "react-router-dom";
-import WorkExperienceCard from "../components/Cards/WorkExperienceCard";
 import "./styles.css";
+import { ExperienceTimeline } from "../components/Icons/ExperienceTimeline";
 
 const Homepage: FC = (): ReactElement => {
   return (
-    <main className="flex  flex-col max-w-7xl mx-auto p-5">
-      <section className="interTypo">
-        <div className="">
-          <h1 className="srcCodeTypo text-left "> Front end developer</h1>
-        </div>
-        <div className="text-left  ">
-          <p>
-            I'm Julian Magra, a frontend developer passionate about learning and
-            working on projects that can be beneficial to the world at large.
-          </p>
-          <p>See more and discover we can do a lot together!</p>
+    <main className="flex  flex-col max-w-7xl mx-auto p-5 text-gray-700 ">
+      <section className="p-2 interTypo flex flex-col lg:flex-row mt-10 ">
+        <div>
+          <div className="text-left  lg:max-w-prose  ">
+            <h1 className="srcCodeTypo text-4xl sm:text-3xl md:text-2xl lg:text-xl xl:text-lg font-bold ">
+              Front end developer
+            </h1>
+            <p className="text-left  mt-10 ">
+              I'm Julian Magra, + 2 years of experience as a frontend developer.
+              I'm passionate about learning and working on projects that can be
+              beneficial to the world at large.
+            </p>
+            <p className="text-left mt-2">
+              See more and discover we can do a lot together!
+            </p>
+          </div>
 
-          <img
-            src="https://images.unsplash.com/photo-1715090156594-aaa3ed5900b9?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-            className=" max-w-sm max-h-sm"
-          />
-        </div>
-
-        <div className="flex  max-w-7xl  p-2">
-          <Link
-            target="_blank"
-            to={"https://github.com/julianmagra"}
-            className="rounded-md  shadow-md  border-b border-b-gray-300 mx-auto "
-          >
-            GitHub
-          </Link>
-          <Link
-            target="_blank"
-            to={"https://gitlab.com/julianmagra"}
-            className="rounded-md  shadow-md  border-b border-b-gray-300 mx-auto "
-          >
-            Gitlab
-          </Link>
-          <Link
-            target="_blank"
-            to={"https://www.linkedin.com/in/julianmagra"}
-            className="rounded-md  shadow-md  border-b border-b-gray-300 mx-auto "
-          >
-            Linkedin
-          </Link>
-          <Link
-            target="_blank"
-            to={"https://twitter.com/JulianNicolas26"}
-            className="rounded-md  shadow-md  border-b border-b-gray-300 mx-auto "
-          >
-            X
-          </Link>
-          <Link
-            target="_blank"
-            to={"https://www.instagram.com/julianmagra"}
-            className="rounded-md  shadow-md  border-b border-b-gray-300 mx-auto "
-          >
-            Instagram
-          </Link>
-          <Link
-            target="_blank"
-            to={"https://www.youtube.com/@julianmagra"}
-            className="rounded-md  shadow-md  border-b border-b-gray-300 mx-auto "
-          >
-            Youtube
-          </Link>
-          <Link
-            target="_blank"
-            to={
-              "https://stackoverflow.com/users/24884113/julian-nicolas-magra?tab=profile"
-            }
-            className="rounded-md  shadow-md  border-b border-b-gray-300 mx-auto "
-          >
-            StackOverflow
-          </Link>
-          {/*   Codepen Dribbble Steam Unsplash Daily.dev Producthunt Stackoverflow
+          <div className=" flex flex-wrap mt-6">
+            <Link
+              target="_blank"
+              to={"https://github.com/julianmagra"}
+              className=" p-2 bg-slate-200 mx-1 rounded-md  shadow-md  border-b border-b-gray-300  "
+            >
+              GitHub
+            </Link>
+            <Link
+              target="_blank"
+              to={"https://gitlab.com/julianmagra"}
+              className="p-2 bg-slate-200 mx-1 rounded-md  shadow-md  border-b border-b-gray-300  "
+            >
+              Gitlab
+            </Link>
+            <Link
+              target="_blank"
+              to={"https://www.linkedin.com/in/julianmagra"}
+              className="p-2 bg-slate-200 mx-1 rounded-md  shadow-md  border-b border-b-gray-300  "
+            >
+              Linkedin
+            </Link>
+            <Link
+              target="_blank"
+              to={"https://twitter.com/JulianNicolas26"}
+              className="p-2 bg-slate-200 mx-1 rounded-md  shadow-md  border-b border-b-gray-300  "
+            >
+              X
+            </Link>
+            <Link
+              target="_blank"
+              to={"https://www.instagram.com/julianmagra"}
+              className="p-2 bg-slate-200 mx-1 rounded-md  shadow-md  border-b border-b-gray-300  "
+            >
+              Instagram
+            </Link>
+            <Link
+              target="_blank"
+              to={"https://www.youtube.com/@julianmagra"}
+              className="p-2 bg-slate-200 mx-1 rounded-md  shadow-md  border-b border-b-gray-300  "
+            >
+              Youtube
+            </Link>
+            <Link
+              target="_blank"
+              to={
+                "https://stackoverflow.com/users/24884113/julian-nicolas-magra?tab=profile"
+              }
+              className="p-2 bg-slate-200 mx-1 rounded-md  shadow-md  border-b border-b-gray-300  "
+            >
+              StackOverflow
+            </Link>
+            {/*   Codepen Dribbble Steam Unsplash Daily.dev Producthunt Stackoverflow
           Codewars */}
+          </div>
+        </div>
+        <div className="mt-10 xl:mt-4 lg:mt-4">
+          <img
+            src="../../public/modul-monitor-with-programming-code-on-screen.png"
+            alt=""
+            className=" xl:w-full lg:w-full lg:h-full mx-auto"
+          />
         </div>
         {/* <p>Resume/CV</p>
         <p>Carrousel abajo exponiendo las redes.</p> */}
       </section>
-      <section className="flex flex-col  p-10 interTypo">
-        <div>
+      <section className=" p-2 interTypo flex flex-col lg:flex-row mt-10 flex-wrap">
+        <div className="text-left">
           <h3 className="srcCodeTypo">Work experience</h3>
         </div>
-        <div className="">
+        <ExperienceTimeline />
+        {/*      <div className="mt-10">
           <WorkExperienceCard />
-        </div>
+        </div> */}
       </section>
-      <section className="interTypo flex">
+      <section className="interTypo  mt-40 ">
         <div>
-          <p>Built with: react - tailwind - 'vercel'</p>
+          <p>Built with:</p>
         </div>
         <div>
-          <p>Copyright © Julian Magra 2024 All rights Reserved</p>
+          <p> react - tailwind - 'vercel'</p>
+        </div>
+        <div>
+          <p className="text-xs">
+            Copyright © Julian Magra 2024 All rights Reserved
+          </p>
         </div>
       </section>
     </main>
--- src/pages/Social.tsx
+++ src/pages/Social.tsx
@@ -1,9 +1,14 @@
 const Social = () => {
   return (
-    <div>
-      <p>
-        In this section you'll know all about me and my experience in IT world!
-      </p>
+    <div className="flex flex-col min-h-screen  bg-slate-500">
+      <div>
+        <h1>This is the main title</h1>
+        <p>And this is his description</p>
+      </div>
+
+      <div>
+        <p>There will probably be a photo here</p>
+      </div>
     </div>
   );
 };
