2d6552d8b8a49483cd47dd225ed15f5fd342a38b
julianmagra <julianmagraestudio@gmail.com>
2024-05-30 20:23:30 -0300

    footer aligned

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,11 +2,11 @@ const Footer = () => {
   return (
     /* corregir alineamiento footer  */
     <footer
-      className="opacity-80 m-4 mt-16 w-full mx-auto container lg:max-w-4xl md:max-w-2xl mb-10 flex justify-center  "
+      className="opacity-80 m-4 mt-16 w-full mx-auto lg:max-w-4xl md:max-w-2xl mb-10 flex justify-center  "
       id="contact"
     >
-      <div className="rounded-lg w-full max-w-screen-xl mx-auto md:flex md:items-center md:justify-between py-4">
-        <p className="text-sm sm:text-center text-zinc-800/90 dark:text-zinc-200/90">
+      <div className="rounded-lg w-full mx-auto md:flex md:items-center md:justify-between py-4">
+        <p className="text-sm text-left md:text-center text-zinc-800/90 dark:text-zinc-200/90">
           Copyright © Julian Magra 2024 All rights Reserved
         </p>
         <div className="flex flex-wrap items-center mt-3 text-sm font-medium dark:text-white/90 sm:mt-0">
