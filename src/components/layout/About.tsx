566fce0d27aa5badd196f19eaf840e4147ea19ac
julianmagra <julianmagraestudio@gmail.com>
2024-05-29 22:42:02 -0300

    styling about

--- src/components/layout/About.tsx
+++ src/components/layout/About.tsx
@@ -1,32 +1,39 @@
+import { BsPersonCheckFill } from "react-icons/bs";
 const AboutMe = () => {
   return (
-    <section id="aboutme" className="p2">
-      <h2 className="text-left srcCodeTypo">About me</h2>
-      <div className="flex flex-col-reverse md:flex-row  mt-10 p-2">
-        <div className="flex flex-col max-w-2xl p2 justify-evenly mr-6 ">
-          <p className="pt-4 ">
+    <section id="aboutme" className=" p-2 ">
+      <h2 className="text-left flex items-center text-3xl font-semibold gap-x-3 text-black/80 dark:text-white ">
+        <BsPersonCheckFill />
+        About me
+      </h2>
+      <div className="flex flex-col-reverse items-center md:flex-row   mt-10 p-2 ">
+        <div className=" [&>p>strong]:text-yellow-300 dark:[&>p>strong]:text-yellow-100 [&>p>strong]:font-normal [&>p>strong]:font-mono text-pretty ">
+          <p className="py-4">
             Me llamo Miguel Ángel pero mis amigos me llaman midu. Empecé en la
-            programación con un Amstrad, tenía 10 años. Actualmente estoy
-            liderando equipos de desarrollo en multinacionales.
+            programación con un Amstrad, tenía 10 años. Actualmente estoy&nbsp;
+            <strong>liderando equipos de desarrollo en multinacionales</strong>.
           </p>
-          <p className="pt-4 pb-4">
-            Algunos de mis éxitos incluyen colaborar con Mozilla para el
-            desarrollo de las primeras apps en su sistema FirefoxOS. Aunque hoy
-            está desaparecido fue un gran avance en el mundo del desarrollo web.
+          <p className="py-4">
+            Algunos de mis éxitos incluyen&nbsp;
+            <strong>
+              colaborar con Mozilla para el desarrollo de las primeras apps en
+              su sistema FirefoxOS
+            </strong>
+            . Aunque hoy está desaparecido fue un gran avance en el mundo del
+            desarrollo web.
           </p>
-          <p className="pt-4 pb-4">
-            Como creador de contenido, cuento con el canal de habla hispana más
-            visto del mundo en la categoría de Software & Game Development en
-            Twitch. Mi objetivo es mejorar la empleabilidad de la comunidad
-            hispana y el acceso a contenido de calidad.
+          <p className="py-4">
+            Como creador de contenido,&nbsp;
+            <strong>
+              cuento con el canal de habla hispana más visto del mundo en la
+              categoría de Software & Game Development en Twitch
+            </strong>
+            . Mi objetivo es mejorar la empleabilidad de la comunidad hispana y
+            el acceso a contenido de calidad.
           </p>
         </div>
-        <div className="p-2 justify-center flex md:block ">
-          <img
-            className="flex min-w-max rounded-xl"
-            src="../../../public/me.jpg"
-            alt="Me"
-          />
+        <div className=" px-10  md:block min-w-max">
+          <img className="rounded-xl" src="../../../public/me.jpg" alt="Me" />
         </div>
       </div>
     </section>
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,16 +1,24 @@
 const Footer = () => {
   return (
-    <section className="mt-40 " id="contact">
-      <div className="flex justify-between items-center pb-10">
-        <p className="text-sm">
+    /* corregir alineamiento footer  */
+    <footer
+      className="opacity-80 m-4 mt-16 w-full mx-auto container lg:max-w-4xl md:max-w-2xl mb-10 flex justify-center  "
+      id="contact"
+    >
+      <div className="rounded-lg w-full max-w-screen-xl mx-auto md:flex md:items-center md:justify-between py-4">
+        <p className="text-sm sm:text-center text-zinc-800/90 dark:text-zinc-200/90">
           Copyright © Julian Magra 2024 All rights Reserved
         </p>
-        <div>
-          <button className="text-md mr-8">Resume</button>
-          <button className="text-md">Contact</button>
+        <div className="flex flex-wrap items-center mt-3 text-sm font-medium dark:text-white/90 sm:mt-0">
+          <button className="text-md mr-8 hover:underline me-4 md:me-6">
+            Resume
+          </button>
+          <button className="text-md hover:underline me-4 md:me-6">
+            Contact
+          </button>
         </div>
       </div>
-    </section>
+    </footer>
   );
 };
 export default Footer;
